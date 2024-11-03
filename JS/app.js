eca61902195fee662cec9630c2baf9a84af2fbad
daniyal <daniyalrouhbakhsh@gmail.com>
2024-11-03 13:00:13 +0330

    isLogin Feature Completed!

--- JS/app.js
+++ JS/app.js
@@ -4,3 +4,12 @@ const formBtn = document.querySelector('#form-btn')
 formBtn.addEventListener('click',e => {
     e.preventDefault()
 })
+
+const isLogin = user => {
+    if(user){
+        return true
+    }
+    else{
+        return false
+    }
+}
